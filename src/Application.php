a83cb4c8fa2d648156aeb89e075a5e1bc555acd1
Dragos Protung <dragos@protung.ro>
2017-12-19 15:46:43 +0100

    Update to Symfony 4

--- src/Application.php
+++ src/Application.php
@@ -4,16 +4,16 @@ declare(strict_types = 1);
 
 namespace Wingu\FluffyPoRobot;
 
-use KevinGH\Amend;
 use Symfony\Component\Console\Application as BaseApplication;
-use Symfony\Component\Console\Helper\HelperSet;
 use Wingu\FluffyPoRobot\Command;
 
 final class Application extends BaseApplication
 {
+    public const VERSION = '0.6.0';
+
     public function __construct()
     {
-        parent::__construct('Fluffy PO Robot', '@git-version@');
+        parent::__construct('Fluffy PO Robot', self::VERSION);
     }
 
     /**
@@ -27,25 +27,6 @@ final class Application extends BaseApplication
         $commands[] = new Command\DownloadCommand();
         $commands[] = new Command\UploadCommand();
 
-        if (('@' . 'git-version@') !== $this->getVersion()) {
-            $command = new Amend\Command('update');
-            $command->setManifestUri('@manifest_url@');
-            $commands[] = $command;
-        }
-
         return $commands;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function getDefaultHelperSet(): HelperSet
-    {
-        $helperSet = parent::getDefaultHelperSet();
-        if (('@' . 'git-version@') !== $this->getVersion()) {
-            $helperSet->set(new Amend\Helper());
-        }
-
-        return $helperSet;
     }
 }
